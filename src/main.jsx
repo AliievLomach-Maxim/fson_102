c3061f41e1c484726c3cc44cf7007d6a8c630935
AliievLomachMaxim <aliev.lomach.maxim@gmail.com>
2024-05-17 20:56:12 +0300

    +

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// App>mount>unmount>mount
+
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<App />
